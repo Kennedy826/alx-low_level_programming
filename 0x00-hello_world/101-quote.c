5340a9b17aa24897037736fe7208c856fe18ec98
Kennedy826 <onyangoken01@gmail.com>
2022-12-08 23:55:21 -0800

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,9 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
+ * main - Prints out the last part of a quote in the standard error.
  *
- * Return: 1 (Succes)
+ * Return: 1 if success.
  */
 int main(void)
 {
