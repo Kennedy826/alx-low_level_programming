6a70cb08e29091c4769b22e43de0ec0838ad0351
Kennedy826 <onyangoken01@gmail.com>
2022-12-10 02:56:02 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - print single digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		printf("%d\n", a);
+		a++;
+	}
+	return (0);
+}
