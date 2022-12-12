771d463ffa263cbc11aeedc4efb9cab1ab3c3258
Kennedy826 <onyangoken01@gmail.com>
2022-12-12 02:18:03 -0800

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdio.h>
+
+/**
+ * main - prints single-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar((a % 10) + '0');
+		if (a == 9)
+				continue;
+				putchar(',');
+				putchar(' ');
+				}
+				putchar('\n');
+				return (0);
+				}
+	
