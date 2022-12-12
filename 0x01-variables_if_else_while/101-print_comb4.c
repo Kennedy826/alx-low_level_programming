0a99d7425626e763e183bb99b264f69cff06b942
Kennedy826 <onyangoken01@gmail.com>
2022-12-12 05:56:26 -0800

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - prints different combinations of three digits
+ *
+ * Returns: Always 0 (Success)
+ */
+int main(void)
+{
+	int tens = '0';
+	int hundreds = '0';
+	int ones = '0';
+
+	for (hundreds = '0'; hundreds <= '9'; hundreds++)
+	{
+		for (tens = '0'; tens <= '9'; tens++)
+		{
+			for (ones = '0'; ones <= '9'; ones++)
+			{
+				if (!(ones == tens) || (tens == hundreds) ||
+						(tens > ones) || (hundreds > tens))
+				{
+					putchar(hundreds);
+					putchar(tens);
+					putchar(ones);
+					if (!(ones == '9' && hundreds == '7' && tens == '8'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
