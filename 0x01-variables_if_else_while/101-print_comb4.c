2615d191da0bc4c99407fba4b80e82d2c128eab5
Kennedy826 <onyangoken01@gmail.com>
2022-12-12 06:07:12 -0800

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,8 +18,7 @@ int main(void)
 		{
 			for (ones = '0'; ones <= '9'; ones++)
 			{
-				if (!(ones == tens) || (tens == hundreds) ||
-						(tens > ones) || (hundreds > tens))
+				if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))
 				{
 					putchar(hundreds);
 					putchar(tens);
